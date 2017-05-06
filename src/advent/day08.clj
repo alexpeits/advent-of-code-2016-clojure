eb9450942b6ef69146ba7765c4fce7c2e0e87a00
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2017-05-06 23:10:54 +0300

    added multimethods for dispatching dsl commands

--- src/advent/day08.clj
+++ src/advent/day08.clj
@@ -21,8 +21,32 @@
                               [:column
                                {:x (Integer/parseInt (second x))
                                 :delta (Integer/parseInt (second (second y)))}])
+                    :axis identity
+                    :cmd identity
                     :size (fn [x y]
                             (zipmap
                              [:x :y]
                              (into [] (map #(Integer/parseInt (second %)) [x y]))))}
                    (dsl expr)))
+
+(defmulti rotate
+  (fn [cmd screen] (first cmd)))
+
+(defmethod rotate :row
+  [[_ args] screen]
+  (println "rotate row"))
+
+(defmethod rotate :column
+  [[_ args] screen]
+  (println "rotate column"))
+
+(defmulti draw
+  (fn [cmd screen] (first cmd)))
+
+(defmethod draw :rect
+  [[_ coords] screen]
+  (println "draw rect"))
+
+(defmethod draw :rotate
+  [[_ cmd] screen]
+  (rotate cmd screen))
