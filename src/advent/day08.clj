dff4e0661da4f5ddc3ab63aa33f87f133184eb33
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2017-05-07 19:56:07 +0300

    completed day 8

--- src/advent/day08.clj
+++ src/advent/day08.clj
@@ -4,8 +4,21 @@
             [clojure.set :as s]
             [instaparse.core :as insta]))
 
+;; resource
 (def data (str/split-lines (read-resource "day08.txt")))
 
+;; screen
+(def on \#)
+(def off \.)
+(def blank-screen (vec (repeat 6 (vec (repeat 50 off)))))
+
+(defn draw-screen [screen]
+  (->> screen
+       (map str/join)
+       (str/join "\n")
+       println))
+
+;; cfg
 (def grammar (read-resource "day08_grammar.txt"))
 (def dsl
   (insta/parser
@@ -29,24 +42,56 @@
                              (into [] (map #(Integer/parseInt (second %)) [x y]))))}
                    (dsl expr)))
 
+;; grammar->method dispatching
+(defn -rotate [arr delta]
+  (let [c (count arr)
+        r (- c (mod delta c))]
+    (vec (concat (drop r arr) (take r arr)))))
+
+(defn -transpose [v]
+  (apply mapv vector v))
+
 (defmulti rotate
   (fn [cmd screen] (first cmd)))
 
 (defmethod rotate :row
-  [[_ args] screen]
-  (println "rotate row"))
+  [[_ {:keys [y delta]}] screen]
+  (update-in screen [y] -rotate delta)
+  )
 
 (defmethod rotate :column
-  [[_ args] screen]
-  (println "rotate column"))
+  [[_ {:keys [x delta]}] screen]
+  (-transpose
+   (update-in (-transpose screen)
+              [x]
+              -rotate
+              delta)))
 
 (defmulti draw
   (fn [cmd screen] (first cmd)))
 
 (defmethod draw :rect
-  [[_ coords] screen]
-  (println "draw rect"))
+  [[_ {:keys [x y]}] screen]
+  (->> (for [xs (range x) ys (range y)] [ys xs])
+       (reduce #(assoc-in %1 %2 on) screen)))
 
 (defmethod draw :rotate
   [[_ cmd] screen]
   (rotate cmd screen))
+
+
+;; solution
+
+(defn solution-a []
+  (->> data
+       (map parse)
+       (reduce #(draw %2 %1) blank-screen)
+       (apply concat)
+       (filter #(= % on))
+       (count)))
+
+(defn solution-b []
+  (->> data
+       (map parse)
+       (reduce #(draw %2 %1) blank-screen)
+       draw-screen))
