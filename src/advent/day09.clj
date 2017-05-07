9afb6f6bb43ce3162d5adf9ca3dee2d18bd3d5d1
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2017-05-08 00:45:24 +0300

    half day 9

---
+++ src/advent/day09.clj
@@ -0,0 +1,29 @@
+(ns advent.day09
+  (:use [advent.utils :only [read-resource]])
+  (:require [clojure.string :as str]))
+
+(def data (str/trim (read-resource "day09.txt")))
+
+(def nums-re #"(\d+)x(\d+)")
+
+(defn mapstr [sequence]
+  (map #(apply str %) sequence))
+
+(defn mapint [sequence]
+  (map #(Integer/parseInt %) sequence))
+
+(defn decompress [s]
+  (loop [acc ""
+         s s]
+    (if (empty? s)
+      acc
+      (let [[a b] (mapstr (split-at (str/index-of s \() s))
+            [n r] (mapint (rest (re-find nums-re (apply str (take 20 b)))))
+            [_ tmp] (mapstr (split-at (inc (str/index-of b \))) b))
+            [rep rm] (mapstr (split-at n tmp))]
+        (recur
+         (str acc a (apply str (repeat r rep)))
+         rm)))))
+
+(defn solution-a []
+  (->> data decompress count))
