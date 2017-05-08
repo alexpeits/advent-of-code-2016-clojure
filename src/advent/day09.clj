f1525d27c7d7fe415c79b7db9e29cc0d764d7a3a
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2017-05-08 12:16:48 +0300

    finished day 9

--- src/advent/day09.clj
+++ src/advent/day09.clj
@@ -6,6 +6,9 @@
 
 (def nums-re #"(\d+)x(\d+)")
 
+(defn has-marker? [s]
+  (re-find nums-re s))
+
 (defn mapstr [sequence]
   (map #(apply str %) sequence))
 
@@ -13,8 +16,7 @@
   (map #(Integer/parseInt %) sequence))
 
 (defn decompress [s]
-  (loop [acc ""
-         s s]
+  (loop [acc "", s s]
     (if (empty? s)
       acc
       (let [[a b] (mapstr (split-at (str/index-of s \() s))
@@ -25,5 +27,22 @@
          (str acc a (apply str (repeat r rep)))
          rm)))))
 
+(defn decompress-count [s]
+  (loop [acc 0, s s]
+    (if (or (empty? s)
+            (not (has-marker? s)))
+      (+ acc (count s))
+      (let [[a b] (mapstr (split-at (str/index-of s \() s))
+            [n r] (mapint (rest (re-find nums-re (apply str (take 20 b)))))
+            [_ tmp] (mapstr (split-at (inc (str/index-of b \))) b))
+            [rep rm] (mapstr (split-at n tmp))]
+        (recur
+         (+ acc (count a) (* r (decompress-count rep)))
+         rm)))))
+
+
 (defn solution-a []
   (->> data decompress count))
+
+(defn solution-b []
+  (decompress-count data))
